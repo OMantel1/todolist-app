771a8c358566449f8e4fc400caa67b02223775d3
omantel <oliviermantel@gmail.com>
2020-09-24 11:10:24 +0200

    Add header - add header - add background image with blur effet

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div class="background-image"></div>
+    <div class="header">
+      <h1 class="header__title">To Do List</h1>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+
 <style lang="scss">
+.header {
+  position: relative;
+  color: white;
+  height: 150px;
+  padding: 16px;
+
+  &__title {
+    letter-spacing: 8px;
+  }
+  a {
+    color: black;
+    text-decoration: none;
+    &.router-link-exact-active {
+      color: white;
+      font-weight: bold;
+      text-decoration: underline;
+    }
+  }
+}
+
+.background-image {
+  width: 100vw;
+  height: 150px;
+  position: absolute;
+  background-color: rgb(138, 138, 138);
+  background-image: url("./assets/notepad-3693309_1920.jpg");
+  background-size: cover;
+  background-position: 50% 60%;
+  filter: blur(1px);
+  -webkit-filter: blur(8px);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
